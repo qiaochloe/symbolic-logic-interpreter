bed72ba5d01653514ef386bb125291a85d6527db
Chloe Qiao <qiaochloe@gmail.com>
2022-07-08 22:47:44 -0700

    Create stmt.py

---
+++ stmt.py
@@ -0,0 +1,13 @@
+class Stmt:
+    pass
+
+
+class Expression(Stmt):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def accept(self, visitor):
+        return visitor.visit_expression_stmt(self)
+
+    def __str__(self):
+        return f"{self.expr}"
