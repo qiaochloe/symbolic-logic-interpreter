f80887303337bed0104018e08c4e5603ba75325c
Chloe Qiao <qiaochloe@gmail.com>
2022-07-08 22:53:34 -0700

    Minor edits

--- token.py
+++ token.py
@@ -2,8 +2,8 @@ class Token:
     def __init__(self, type_, lexeme, literal, line):
         self.type_ = type_
         self.lexeme = lexeme
-        self.literal = literal  # NOTE: Why does this exist?
+        self.literal = literal
         self.line = line
 
     def __str__(self):
-        return f"{self.lexeme}"
+        return f"{self.type_} {self.lexeme} {self.literal}"
